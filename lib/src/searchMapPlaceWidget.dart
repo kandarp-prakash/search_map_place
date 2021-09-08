8ac0de62e3ca01c0573dcf3947074b14e1de354a
Kandarp-PSSPL <63350348+kandarp-prakash@users.noreply.github.com>
2021-09-08 18:35:40 +0530

    Update searchMapPlaceWidget.dart

--- lib/src/searchMapPlaceWidget.dart
+++ lib/src/searchMapPlaceWidget.dart
@@ -355,6 +355,8 @@ class _SearchMapPlaceWidgetState extends State<SearchMapPlaceWidget> with Ticker
   /// Will listen for input changes every 0.5 seconds, allowing us to make API requests only when the user stops typing.
   void customListener() {
     Future.delayed(Duration(milliseconds: 500), () {
+       if (!this.mounted)
+        return;
       setState(() => _tempInput = _textEditingController.text);
       customListener();
     });
